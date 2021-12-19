92187884259f0d9562a0c61be4c479ea905513cb
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2021-12-20 02:31:13 +0900

    [ADD] Tailwind and some packages

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  mode: process.env.NODE_ENV ? 'jit' : undefined,
+}
