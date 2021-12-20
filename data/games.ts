59a91e6b67fe32d028557181639859d002ff4b0a
Yoan Saint Juste <yoan.saint-juste@epitech.eu>
2021-12-21 02:10:36 +0900

    [WIP] Returnal game but images arent there yet

--- data/games.ts
+++ data/games.ts
@@ -16,10 +16,29 @@ const games: Array<IGame> = [
     ],
     price: 69.99,
     releaseDate: "Already released",
-    stars: 4,
+    stars: 5,
     thumbnail:
       "https://culturedvultures.com/wp-content/uploads/2021/02/Rift-Apart-Box-Art.jpg",
     id: "g4me-rat-ps5",
+  },
+  {
+    name: "Returnal",
+    avaiableOn: [getConsole("PS5")],
+    description: "Break the cycle of chaos on an always-changing alien planet.",
+    extendedDescription:
+      "After crash-landing on this shape-shifting world, Selene must search through the barren landscape of an ancient civilization for her escape. Isolated and alone, she finds herself fighting tooth and nail for survival. Again and again, she’s defeated – forced to restart her journey every time she dies.",
+    images: [
+      "https://sm.ign.com/ign_me/gallery/r/ratchet-cl/ratchet-clank-rift-apart-key-art-box-art-pre-order-bonuses-d_dz8f.jpg",
+      "https://cdn.arstechnica.net/wp-content/uploads/2021/06/Ratchet-_-Clank_-Rift-Apart_20210526113947.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgugMBLHOMpD-UxPzGdJtupEX_dlamppcgMw&usqp=CAU",
+      "https://cdn.mos.cms.futurecdn.net/Gfq3B4WT8gwNyJRQPw3nW4.jpg",
+    ],
+    price: 49.99,
+    releaseDate: "Already released",
+    stars: 4,
+    thumbnail:
+      "https://media.direct.playstation.com/is/image/sierialto/returnal-ps5-game-box-front",
+    id: "g4me-ret-ps5",
   },
 ];
 
